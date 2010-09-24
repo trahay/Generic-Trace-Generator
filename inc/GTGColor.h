67af3b1d73ceb1863751f7318a86010aac94f090
coulomb <coulomb@0c38aafd-2db3-40e8-87a3-ec7d7f65c264>
2010-09-24 11:01:58 +0000

    Oubli de l'ajout des couleurs pour GTG
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gtg/trunk@30 0c38aafd-2db3-40e8-87a3-ec7d7f65c264

---
+++ inc/GTGColor.h
@@ -0,0 +1,122 @@
+#ifndef _GTGCOLOR_
+#define _GTGCOLOR_
+
+
+/**
+ * \defgroup color Defined colors for GTG
+ */
+
+/**
+ * \ingroup color
+ * \brief Black color
+ */
+#define GTG_BLACK      0
+/**
+ * \ingroup color
+ * \brief Red color
+ */
+#define GTG_RED        1
+/**
+ * \ingroup color
+ * \brief Green color
+ */
+#define GTG_GREEN      2
+/**
+ * \ingroup color
+ * \brief Blue color
+ */
+#define GTG_BLUE       3
+/**
+ * \ingroup color
+ * \brief White color
+ */
+#define GTG_WHITE      5
+/**
+ * \ingroup color
+ * \brief Teal color
+ */
+#define GTG_TEAL       6
+/**
+ * \ingroup color
+ * \brief Dark grey color
+ */
+#define GTG_DARKGREY   7
+/**
+ * \ingroup color
+ * \brief Yellow color
+ */
+#define GTG_YELLOW     8
+/**
+ * \ingroup color
+ * \brief Purple color
+ */
+#define GTG_PURPLE     9
+/**
+ * \ingroup color
+ * \brief Light brown color
+ */
+#define GTG_LIGHTBROWN 10
+/**
+ * \ingroup color
+ * \brief Light grey color
+ */
+#define GTG_LIGHTGREY  11
+/**
+ * \ingroup color
+ * \brief Dark blue color
+ */
+#define GTG_DARKBLUE   12
+/**
+ * \ingroup color
+ * \brief Pink color
+ */
+#define GTG_PINK       13
+/**
+ * \ingroup color
+ * \brief Dark pink color
+ */
+#define GTG_DARKPINK   14
+/**
+ * \ingroup color
+ * \brief Sea blue color
+ */
+#define GTG_SEABLUE    15
+/**
+ * \ingroup color
+ * \brief Kaki color
+ */
+#define GTG_KAKI       16
+/**
+ * \ingroup color
+ * \brief Red blood color
+ */
+#define GTG_REDBLOOD   17
+/**
+ * \ingroup color
+ * \brief Brown color
+ */
+#define GTG_BROWN      18
+/**
+ * \ingroup color
+ * \brief Grenat color
+ */
+#define GTG_GRENAT     19
+/**
+ * \ingroup color
+ * \brief Orange color
+ */
+#define GTG_ORANGE     20
+/**
+ * \ingroup color
+ * \brief Mauve color
+ */
+#define GTG_MAUVE      21
+/**
+ * \ingroup color
+ * \brief Light pink color
+ */
+#define GTG_LIGHTPINK  22
+
+
+
+#endif
