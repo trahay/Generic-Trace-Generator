7cbcd5d220bc61e0fd5e636884557fc5339da751
ftrahay <ftrahay@0c38aafd-2db3-40e8-87a3-ec7d7f65c264>
2010-09-25 14:05:02 +0000

    * fix wrong handling of RGB values * add missing headers to Makefile.am * add default colors
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gtg/trunk@35 0c38aafd-2db3-40e8-87a3-ec7d7f65c264

--- inc/GTGColor.h
+++ inc/GTGColor.h
@@ -20,8 +20,8 @@ typedef struct gtg_color {
  * 0x00rrggbb
  */
 #define GTG_COLOR_BLUE_POS  0
-#define GTG_COLOR_GREEN_POS 1
-#define GTG_COLOR_RED_POS   2
+#define GTG_COLOR_GREEN_POS 8
+#define GTG_COLOR_RED_POS   16
 
 #define GTG_COLOR_BLUE_MASK  (0x000000ff << GTG_COLOR_BLUE_POS)
 #define GTG_COLOR_GREEN_MASK (0x000000ff << GTG_COLOR_GREEN_POS)
@@ -44,133 +44,212 @@ typedef struct gtg_color {
  * \ingroup color
  * \brief Black color
  */
-#define GTG_BLACK      0
 #define GTG_BLACK_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(0, 0, 0)
 #define GTG_COLOR_BLACK(var) \
-	gtg_color_t var = {.color_name = "BLACK"; rgb = GTG_BLACK_RGB}
+	gtg_color_t var = {.color_name = "BLACK", .rgb = GTG_BLACK_RGB}
 
 /**
  * \ingroup color
  * \brief Red color
  */
-#define GTG_RED        1
 #define GTG_RED_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(255, 0, 0)
 #define GTG_COLOR_RED(var) \
-	gtg_color_t var = {.color_name = "RED"; rgb = GTG_RED_RGB}
+	gtg_color_t var = {.color_name = "RED", .rgb = GTG_RED_RGB}
 
 /**
  * \ingroup color
  * \brief Green color
  */
-#define GTG_GREEN      2
 #define GTG_GREEN_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(0, 255, 0)
 #define GTG_COLOR_GREEN(var) \
-	gtg_color_t var = {.color_name = "GREEN"; rgb = GTG_GREEN_RGB}
+	gtg_color_t var = {.color_name = "GREEN", .rgb = GTG_GREEN_RGB}
 
 /**
  * \ingroup color
  * \brief Blue color
  */
-#define GTG_BLUE       3
 #define GTG_BLUE_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(0, 0, 255)
 #define GTG_COLOR_BLUE(var) \
-	gtg_color_t var = {.color_name = "BLUE"; rgb = GTG_BLUE_RGB}
+	gtg_color_t var = {.color_name = "BLUE", .rgb = GTG_BLUE_RGB}
 
 /**
  * \ingroup color
  * \brief White color
  */
-#define GTG_WHITE      5
-/* todo: get the RGB value for the other colors */
 #define GTG_WHITE_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(255, 255, 255)
 #define GTG_COLOR_WHITE(var) \
-	gtg_color_t var = {.color_name = "WHITE"; rgb = GTG_WHITE_RGB}
+	gtg_color_t var = {.color_name = "WHITE", .rgb = GTG_WHITE_RGB}
 
 /**
  * \ingroup color
  * \brief Teal color
  */
-#define GTG_TEAL       6
+#define GTG_TEAL_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(0, 255, 255)
+#define GTG_COLOR_TEAL(var) \
+	gtg_color_t var = {.color_name = "TEAL", .rgb = GTG_TEAL_RGB}
+
 /**
  * \ingroup color
  * \brief Dark grey color
  */
-#define GTG_DARKGREY   7
+#define GTG_DARKGREY_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(85, 85, 85)
+#define GTG_COLOR_DARKGREY(var) \
+	gtg_color_t var = {.color_name = "DARKGREY", .rgb = GTG_DARKGREY_RGB}
+
 /**
  * \ingroup color
  * \brief Yellow color
  */
-#define GTG_YELLOW     8
+#define GTG_YELLOW_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(255, 255, 0)
+#define GTG_COLOR_YELLOW(var) \
+	gtg_color_t var = {.color_name = "YELLOW", .rgb = GTG_YELLOW_RGB}
+
 /**
  * \ingroup color
  * \brief Purple color
  */
-#define GTG_PURPLE     9
+#define GTG_PURPLE_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(153, 25, 230)
+#define GTG_COLOR_PURPLE(var) \
+	gtg_color_t var = {.color_name = "PURPLE", .rgb = GTG_PURPLE_RGB}
+
 /**
  * \ingroup color
  * \brief Light brown color
  */
-#define GTG_LIGHTBROWN 10
+#define GTG_LIGHTBROWN_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(170, 130, 130)
+#define GTG_COLOR_LIGHTBROWN(var) \
+	gtg_color_t var = {.color_name = "LIGHTBROWN", .rgb = GTG_LIGHTBROWN_RGB}
+
+
 /**
  * \ingroup color
  * \brief Light grey color
  */
-#define GTG_LIGHTGREY  11
+#define GTG_LIGHTGREY_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(200, 200, 200)
+#define GTG_COLOR_LIGHTGREY(var) \
+	gtg_color_t var = {.color_name = "LIGHTGREY", .rgb = GTG_LIGHTGREY_RGB}
+
+
 /**
  * \ingroup color
  * \brief Dark blue color
  */
-#define GTG_DARKBLUE   12
+#define GTG_DARKBLUE_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(0, 0, 80)
+#define GTG_COLOR_DARKBLUE(var) \
+	gtg_color_t var = {.color_name = "DARKBLUE", .rgb = GTG_DARKBLUE_RGB}
+
+
 /**
  * \ingroup color
  * \brief Pink color
  */
-#define GTG_PINK       13
+#define GTG_PINK_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(255, 0, 255)
+#define GTG_COLOR_PINK(var) \
+	gtg_color_t var = {.color_name = "PINK", .rgb = GTG_PINK_RGB}
+
 /**
  * \ingroup color
  * \brief Dark pink color
  */
-#define GTG_DARKPINK   14
+#define GTG_DARKPINK_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(180, 80, 180)
+#define GTG_COLOR_DARKPINK(var) \
+	gtg_color_t var = {.color_name = "DARKPINK", .rgb = GTG_DARKPINK_RGB}
+
 /**
  * \ingroup color
  * \brief Sea blue color
  */
-#define GTG_SEABLUE    15
+#define GTG_SEABLUE_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(25, 128, 200)
+#define GTG_COLOR_SEABLUE(var) \
+	gtg_color_t var = {.color_name = "SEABLUE", .rgb = GTG_SEABLUE_RGB}
+
+
 /**
  * \ingroup color
  * \brief Kaki color
  */
-#define GTG_KAKI       16
+#define GTG_KAKI_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(80, 100, 25)
+#define GTG_COLOR_KAKI(var) \
+	gtg_color_t var = {.color_name = "KAKI", .rgb = GTG_KAKI_RGB}
+
+
 /**
  * \ingroup color
  * \brief Red blood color
  */
-#define GTG_REDBLOOD   17
+#define GTG_REDBLOOD_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(200, 25, 25)
+#define GTG_COLOR_REDBLOOD(var) \
+	gtg_color_t var = {.color_name = "REDBLOOD", .rgb = GTG_REDBLOOD_RGB}
+
 /**
  * \ingroup color
  * \brief Brown color
  */
-#define GTG_BROWN      18
+#define GTG_BROWN_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(100, 25, 25)
+#define GTG_COLOR_BROWN(var) \
+	gtg_color_t var = {.color_name = "BROWN", .rgb = GTG_BROWN_RGB}
+
+
 /**
  * \ingroup color
  * \brief Grenat color
  */
-#define GTG_GRENAT     19
+#define GTG_GRENAT_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(100, 0, 80)
+#define GTG_COLOR_GRENAT(var) \
+	gtg_color_t var = {.color_name = "GRENAT", .rgb = GTG_GRENAT_RGB}
+
+
 /**
  * \ingroup color
  * \brief Orange color
  */
-#define GTG_ORANGE     20
+#define GTG_ORANGE_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(255, 160, 0)
+#define GTG_COLOR_ORANGE(var) \
+	gtg_color_t var = {.color_name = "ORANGE", .rgb = GTG_ORANGE_RGB}
+
+
 /**
  * \ingroup color
  * \brief Mauve color
  */
-#define GTG_MAUVE      21
+#define GTG_MAUVE_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(128, 0, 255)
+#define GTG_COLOR_MAUVE(var) \
+	gtg_color_t var = {.color_name = "MAUVE", .rgb = GTG_MAUVE_RGB}
+
+
 /**
  * \ingroup color
  * \brief Light pink color
  */
-#define GTG_LIGHTPINK  22
+#define GTG_LIGHTPINK_RGB      (gtg_rgb_color_t) GTG_COLOR_SET_COLOR(255, 128, 255)
+#define GTG_COLOR_LIGHTPINK(var) \
+	gtg_color_t var = {.color_name = "LIGHTPINK", .rgb = GTG_LIGHTPINK_RGB}
+
+/**
+ * declare a default gtg_color_t variables (GTG_BLACK, GTG_RED, etc.)
+ */
+static GTG_COLOR_BLACK      (GTG_BLACK);
+static GTG_COLOR_RED        (GTG_RED);
+static GTG_COLOR_GREEN      (GTG_GREEN);
+static GTG_COLOR_BLUE       (GTG_BLUE);
+static GTG_COLOR_WHITE      (GTG_WHITE);
+static GTG_COLOR_TEAL       (GTG_TEAL);
+static GTG_COLOR_DARKGREY   (GTG_DARKGREY);
+static GTG_COLOR_YELLOW     (GTG_YELLOW);
+static GTG_COLOR_PURPLE     (GTG_PURPLE);
+static GTG_COLOR_LIGHTBROWN (GTG_LIGHTBROWN);
+static GTG_COLOR_LIGHTGREY  (GTG_LIGHTGREY);
+static GTG_COLOR_DARKBLUE   (GTG_DARKBLUE);
+static GTG_COLOR_PINK       (GTG_PINK);
+static GTG_COLOR_DARKPINK   (GTG_DARKPINK);
+static GTG_COLOR_SEABLUE    (GTG_SEABLUE);
+static GTG_COLOR_KAKI       (GTG_KAKI);
+static GTG_COLOR_REDBLOOD   (GTG_REDBLOOD);
+static GTG_COLOR_BROWN      (GTG_BROWN);
+static GTG_COLOR_GRENAT     (GTG_GRENAT);
+static GTG_COLOR_ORANGE     (GTG_ORANGE);
+static GTG_COLOR_MAUVE      (GTG_MAUVE);
+static GTG_COLOR_LIGHTPINK  (GTG_LIGHTPINK);
 
 
 
